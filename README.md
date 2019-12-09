3a2063ef3ee78593f57a7c33a7a09e273f67808d
codepuncher <lee.hanbury-pickett@itineris.co.uk>
2019-12-09 10:16:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What does this plugin do?
+Currently, this only adds the `HTTP_BASIC_AUTH_USERNAME` AND `HTTP_BASIC_AUTH_PASSWORD` constants.
+
+# Installation
+Run `composer require itinerisltd/searchwp-config` in the Bedrock directory.
+
+# Usage
+Perform the above steps - that's it!
---
+++ searchwp-config.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Plugin Name:     Itineris SearchWP Security Configuration
+ * Plugin URI:      https://github.com/ItinerisLtd/searchwp-config/
+ * Description:     Sets default SearchWP configurations
+ * Version:         0.1.0
+ * Author:          Itineris Limited
+ * Author URI:      https://www.itineris.co.uk/
+ * Text Domain:     searchwp-config
+ */
+
+declare(strict_types=1);
+
+namespace Itineris\SearchWPConfig;
+
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+    die;
+}
+
+add_filter('searchwp_basic_auth_creds', __NAMESPACE__ '\\setBasicAuthCredentials');
+
+function setBasicAuthCredentials(): array
+{
+    if (defined('HTTP_BASIC_AUTH_USERNAME') && defined('HTTP_BASIC_AUTH_PASSWORD')) {
+        return [
+            'username' => HTTP_BASIC_AUTH_USERNAME,
+            'password' => HTTP_BASIC_AUTH_PASSWORD,
+        ];
+    }
+
+    return [
+        'username' => '',
+        'password' => '',
+    ];
+}
