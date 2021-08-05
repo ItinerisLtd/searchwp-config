d04b2475654354f68f3657af24560d301de916df
codepuncher <lee.hanbury@gmail.com>
2021-08-05 11:23:56 +0100

    refactor: extract basic auth code to own class

---
+++ src/BasicAuth.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Itineris\SearchWpConfig;
+
+final class BasicAuth
+{
+    /**
+     * @var string Basic auth username
+     */
+    protected string $username;
+
+    /**
+     * @var string Basic auth password
+     */
+    protected string $password;
+
+    public static function register(): void
+    {
+        if (! defined('HTTP_BASIC_AUTH_USERNAME') || ! defined('HTTP_BASIC_AUTH_PASSWORD')) {
+            return;
+        }
+
+        $instance = new self(HTTP_BASIC_AUTH_USERNAME, HTTP_BASIC_AUTH_PASSWORD);
+
+        add_filter('searchwp_basic_auth_creds', [$instance, 'setBasicAuthCredentials']);
+        add_filter('searchwp\indexer\http_basic_auth_credentials', [$instance, 'setBasicAuthCredentials']);
+        add_filter('cron_request', [$instance, 'setCronRequestBasicAuthCredentials'], 999);
+    }
+
+    public function __construct(string $username, string $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function setBasicAuthCredentials(): array
+    {
+        return [
+            'username' => $this->username,
+            'password' => $this->password,
+        ];
+    }
+
+    public function setCronRequestBasicAuthCredentials(array $cron_request): array
+    {
+        if (! isset($cron_request['args']['headers'])) {
+            $cron_request['args']['headers'] = [];
+        }
+
+        if (isset($cron_request['args']['headers']['Authorization'])) {
+            return $cron_request;
+        }
+
+        $cron_request['args']['headers']['Authorization'] = sprintf(
+            'Basic %s',
+            // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+            base64_encode($this->username . ':' . $this->password)
+        );
+
+        return $cron_request;
+    }
+}
--- src/Plugin.php
+++ src/Plugin.php
@@ -8,48 +8,9 @@ final class Plugin
 {
     public static function register(): void
     {
-        add_filter('searchwp_basic_auth_creds', [self::class, 'setBasicAuthCredentials']);
-        add_filter('searchwp\indexer\http_basic_auth_credentials', [self::class, 'setBasicAuthCredentials']);
-        add_filter('cron_request', [self::class, 'setCronRequestBasicAuthCredentials'], 999);
+        BasicAuth::register();
+
         add_filter('searchwp\indexer\alternate', [self::class, 'setIndexerAlternateCondition']);
-    }
-
-    public static function setBasicAuthCredentials(): array
-    {
-        if (defined('HTTP_BASIC_AUTH_USERNAME') && defined('HTTP_BASIC_AUTH_PASSWORD')) {
-            return [
-                'username' => HTTP_BASIC_AUTH_USERNAME,
-                'password' => HTTP_BASIC_AUTH_PASSWORD,
-            ];
-        }
-
-        return [
-            'username' => '',
-            'password' => '',
-        ];
-    }
-
-    public static function setCronRequestBasicAuthCredentials(array $cron_request): array
-    {
-        if (! defined('HTTP_BASIC_AUTH_USERNAME') || ! defined('HTTP_BASIC_AUTH_PASSWORD')) {
-            return $cron_request;
-        }
-
-        if (! isset($cron_request['args']['headers'])) {
-            $cron_request['args']['headers'] = [];
-        }
-
-        if (isset($cron_request['args']['headers']['Authorization'])) {
-            return $cron_request;
-        }
-
-        $cron_request['args']['headers']['Authorization'] = sprintf(
-            'Basic %s',
-            // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
-            base64_encode(HTTP_BASIC_AUTH_USERNAME . ':' . HTTP_BASIC_AUTH_PASSWORD)
-        );
-
-        return $cron_request;
     }
 
     /**
